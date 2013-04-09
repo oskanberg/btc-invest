66ffda5ac2760ec79b095070720666fbe1d305a5
ost500 <ost500@cs.york.ac.uk>
2013-04-10 00:00:28 +0100

    sceletal crisis daemon implementation

--- brains.py
+++ brains.py
@@ -6,14 +6,40 @@ import datetime
 import time
 import os
 
-ac = public_api_client()
+class crisis_daemon():
+    
+    def __init__(self, public_api_client, baseline_price=None):
+        self.alive = True
+        self.risk_level = 0.0
+        self.public_api_client = public_api_client
+        self.baseline_price = baseline_price
+        if self.baseline_price == None:
+            self.baseline_price = self.public_api_client.get_last_price()
+        print 'crisis_daemon:\t\tbaseline_price = %f' % self.baseline_price
+    
+    def update_tracker(self):
+        latest_trade = self.public_api_client.get_last_price()
+        if self.trades_record:
+            if latest_trade != self.trades_record[-1]:
+                self.trades_record.append(latest_trade)
+            else:
+                print 'crisis_daemon:\t\tLast trade price has not changed.'
+        else:
+            self.trades_record.append(latest_trade)
+    
+    def evaluate_risk_level(self):
+        # last five minutes
+        trades = self.public_api_client.transactions(300)
+        # TODO
+        
 
-while True:
-    ticker = ac.ticker()
-    os.system(['clear', 'cls'][os.name == 'nt'])
-    print ' Updated: %s' % datetime.datetime.now()
-    print '=====================================\n'
-    for key in ticker.keys():
-        print '%s:\t%s' % (key, ticker[key])
-    time.sleep(10)
+    def run(self):
+        while self.alive:
+            self.evaluate_risk_level()
+            time.sleep(5)
 
+
+if __name__ == "__main__":
+    public_client = public_api_client()
+    crisis_d = crisis_daemon(public_client)
+    crisis_d.run()
