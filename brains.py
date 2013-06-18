5f07853a79fe7d44629c578d9c9387c04df1cb70
oskanberg <ost500@york.ac.uk>
2013-06-18 20:29:05 +0100

    typo fix

--- brains.py
+++ brains.py
@@ -77,7 +77,7 @@ class brain(object):
             else:
                 if price_disparity > 3:
                     self.logger.debug('large price disparity - committing')
-                    api = self.kernel.get_api([)'bitstamp_api')
+                    api = self.kernel.get_api('bitstamp_api')
                     api.buy_at_market_price_with_limit(1, bitstamp_price + (price_disparity - 3))
                 else:
                     self.logger.debug('not committed and not committing')
